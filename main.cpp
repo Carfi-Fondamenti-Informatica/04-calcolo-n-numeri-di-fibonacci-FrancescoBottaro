aa5d6f705a7c2b102e55ba9630914970195fddee
FrancescoBottaro <117718788+FrancescoBottaro@users.noreply.github.com>
2022-11-22 15:39:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0;
+    int Fib1=1, Fib2=1, numeroFibonacci=0;
+
+    cin>>n;
+
+    if (n < 2){
+       cout<< "errore"<<endl;
+    }
+
+    else{
+       cout<<Fib1<<endl;
+       cout<<Fib2<<endl;
+
+       int i=0;
+       for (i=3 ; i<=n; i++)
+       {
+           numeroFibonacci =  Fib2 + Fib1  ;
+           cout<<numeroFibonacci<<endl;
+           Fib1 = Fib2 ;
+           Fib2 = numeroFibonacci ;
+
+       }
+    }
+    return 0;
 }
